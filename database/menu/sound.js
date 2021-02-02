22ab32a4d991f492cca696591b16edc13485b7e2
RichardKAFA <76983260+RichardKAFA@users.noreply.github.com>
2021-02-02 03:26:18 -0300

    Update sound.js

--- database/menu/sound.js
+++ database/menu/sound.js
@@ -1,1 +1,18 @@
-1
+const sound = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *SOUND*
+  │
+  └─ ❏ ${prefix}tts`
+}
+exports.sound = sound
