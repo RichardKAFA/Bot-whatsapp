cfb983161fd998435d26a75e4f029066b80a5c86
RichardKAFA <76983260+RichardKAFA@users.noreply.github.com>
2021-02-02 03:18:47 -0300

    Update imagemaker.js

--- database/menu/imagemaker.js
+++ database/menu/imagemaker.js
@@ -1,1 +1,54 @@
-1
+
+bruxinhok
+/
+Bot-whatsapp
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Bot-whatsapp/database/menu/imagemaker.js
+@bruxinhok
+bruxinhok Add files via upload
+ 1 contributor
+37 lines (36 sloc)  986 Bytes
+ 
+const imagemaker = (prefix, botName, ownerName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *IMAGE MAKER*
+  │
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}herrypotter
+  └─ ❏ ${prefix}quotemaker`
+}
+exports.imagemaker = imagemaker
