2009b7c4a8e43a860a3208d72cfff837ab24c9e5
RichardKAFA <76983260+RichardKAFA@users.noreply.github.com>
2021-02-02 03:29:42 -0300

    Update tod.js

--- database/menu/tod.js
+++ database/menu/tod.js
@@ -1,1 +1,19 @@
-1
+const tod = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *TOD*
+  │
+  ├─ ❏ ${prefix}trust
+  └─ ❏ ${prefix}dare`
+}
+exports.tod = tod
