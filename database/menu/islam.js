f78722d0240d5c8b4fa49850fc364944d8cc3009
RichardKAFA <76983260+RichardKAFA@users.noreply.github.com>
2021-02-02 03:20:09 -0300

    Update islam.js

--- database/menu/islam.js
+++ database/menu/islam.js
@@ -1,1 +1,18 @@
-1
+const islam = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran`
+}
+exports.islam = islam
