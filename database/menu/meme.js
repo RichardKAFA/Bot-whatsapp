2637cd20b880b90f65f948ca0863af39b39e95bb
RichardKAFA <76983260+RichardKAFA@users.noreply.github.com>
2021-02-02 03:22:07 -0300

    Update meme.js

--- database/menu/meme.js
+++ database/menu/meme.js
@@ -1,1 +1,19 @@
-1
+const meme = (prefix, botName, ownerName) => {
+	return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo`
+}
+exports.meme = meme
